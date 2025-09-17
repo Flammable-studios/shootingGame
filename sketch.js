2975083d4d958dc26630eafd59b05b229e4c2fcb
Your Name <fishpenguin105@gmail.com>
2025-09-17 10:07:44 -0700

    Soundandenemystuff

--- sketch.js
+++ sketch.js
@@ -1,16 +1,27 @@
 // Project Title Shooting Demo
+
 // Your Name Jayson
 
 // player position variables
+
 let pX = 50;
 let pY = 200;
+let enemyX = 550;
+let enemyY = 200;
+let headShot;
 
 // projectile positions
+let projectileXs = [];
+let projectileYs = [];
 
-
+function preload() {
+    soundFormats('mp3')
+    headShot = loadSound('Headshot.mp3')
+}
 function setup() {
     createCanvas(600, 400);
     background(240);
+    rectMode(CENTER);
 }
 
 function draw() {
@@ -21,7 +32,10 @@ function draw() {
 
     drawPlayer(pX, pY);
 
-    // drawProjectiles();
+    drawProjectiles();
+
+
+    rect(enemyX, enemyY, 50, 100)
 }
 
 function drawPlayer(x, y) {
@@ -32,9 +46,11 @@ function drawPlayer(x, y) {
 function movePlayer() {
     if (keyIsDown(83) || keyIsDown(40)) { // w & arrow up key
         pY += 5;
+
     }
     if (keyIsDown(87) || keyIsDown(38)) { // s & arrow down key
         pY -= 5;
+
     }
 }
 
@@ -45,6 +61,31 @@ function keyPressed() {
 //update and draw projectiles
 function drawProjectiles() {
     fill('red');
+    for (let i = 0; i < projectileXs.length; i += 1) {
+        ellipse(projectileXs[i], projectileYs[i], 5, 2);
+        projectileXs[i] += 5;
+        //Delete projectile
+        if (projectileXs[i] > 650) {
+            projectileXs.splice(i, i);
+            projectileYs.splice(i, i);
+        }
+        //collision
+        if (abs(projectileXs[i] - enemyX) < 27.5) {
+            if (abs(projectileYs[i] - enemyY) < 51) {
+                projectileXs.splice(i, i);
+                projectileYs.splice(i, i);
+                background('red');
+                enemyX = random(300, 600);
+                enemyY = random(0, 400);
+                headShot.play();
+            }
+        }
+    }
+}
 
-
-} 
+function mousePressed() {
+    projectileXs.push(pX + 20);
+    projectileYs.push(pY);
+    print(projectileXs);
+    print(projectileYs);
+}
